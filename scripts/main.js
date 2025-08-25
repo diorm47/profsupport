f30f60132e3d14b5489e37100b665149431662d8
diorm47 <dior47m@gmail.com>
2025-08-25 17:17:05 +0500

    carousel, hovers

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.location.hash) {
+    const target = document.querySelector(window.location.hash);
+    if (target) {
+      setTimeout(() => {
+        target.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }, 300);
+    }
+  }
+
+  document.querySelectorAll('a[href^="#"]').forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const id = this.getAttribute("href");
+      const target = document.querySelector(id);
+      if (target) {
+        target.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    });
+  });
+});
+
+//
+function toggleServMenu() {
+  document
+    .querySelector(".service__modal")
+    .classList.toggle("service__modal-active");
+}
